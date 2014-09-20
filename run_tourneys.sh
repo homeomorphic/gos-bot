6b28a046ae5aebf0f0b4ffe603ce4cbab3bc2196
Jochem Berndsen <jochem@functor.nl>
2014-09-20 13:09:06 +0200

    add tourney runner

---
+++ run_tourneys.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+EC2S=`cat ec2s`;
+
+echo "** Killing java"
+for ec2 in $EC2S; do
+    ssh $ec2 killall java
+done
+
+echo "** git commit and build"
+git commit -am"testing bot"
+HEAD=`git log -1 --format=%h`
+./make.sh
+TARGET=bot-$HEAD.jar
+mv bot.jar $TARGET
+
+echo "** starting competition for $TARGET"
+for ec2 in $EC2S; do
+    ./run_tourney_on.sh $ec2 $TARGET > out-$ec2 &
+done
+
+tail -f out-$ec2
+
+
